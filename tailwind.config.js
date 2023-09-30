09c39d6c57fd5b99218c450ebda251117cb6ead2
aiydn <48388034+aiydn@users.noreply.github.com>
2023-09-30 23:35:57 +0200

    update colors & notification position

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,24 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
-}
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["[data-theme=light]"],
+          "base-content": "black",
+        },
+        dark : {
+          ...require("daisyui/src/theming/themes")["[data-theme=dark]"],
+          "primary": "blue",
+          "base-100": "#242424",
+          "base-200": "#1f1f1f",
+          "base-300": "#1a1a1a",
+          "base-content": "#b0b0b0",
 
+
+        },
+      },
+    ],
+  },
+}
